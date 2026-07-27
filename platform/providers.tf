320ba9f852fb8adaf5872e4a931f5b4f690098a1
simson0130 <38636886+simson0130@users.noreply.github.com>
2026-07-27 11:57:52 +0900

    sync: platform providers.tf

--- platform/providers.tf
+++ platform/providers.tf
@@ -1,8 +1,38 @@
+terraform {
+  backend "s3" {
+    bucket         = "eerf-terraform-state-665989470268"
+    key            = "platform/terraform.tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "eerf-terraform-lock"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = var.region
+
+  default_tags {
+    tags = {
+      Service     = "eerf"
+      Department  = "proserve"
+      Environment = "prod"
+      ManagedBy   = "terraform"
+      Project     = "eerf"
+    }
+  }
 }
 
 provider "aws" {
   alias  = "use1"
   region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Service     = "eerf"
+      Department  = "proserve"
+      Environment = "prod"
+      ManagedBy   = "terraform"
+      Project     = "eerf"
+    }
+  }
 }
